bd6542d9a1a89c9a248cb46ba39dd5799c4a55a3
find-jr200 <zifl39.a9dp@gmail.com>
2023-12-08 01:03:47 +0900

    プロジェクト ファイルを追加します。

---
+++ EditFont/Main.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace EditFont
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.PicFontTable = new System.Windows.Forms.PictureBox();
+            this.PicEditFont = new System.Windows.Forms.PictureBox();
+            this.GrpRotation = new System.Windows.Forms.GroupBox();
+            this.RdoR270 = new System.Windows.Forms.RadioButton();
+            this.RdoR180 = new System.Windows.Forms.RadioButton();
+            this.RdoR90 = new System.Windows.Forms.RadioButton();
+            this.RdoR0 = new System.Windows.Forms.RadioButton();
+            this.BtnWriteBack = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.GrpFormat = new System.Windows.Forms.GroupBox();
+            this.RdoCjr = new System.Windows.Forms.RadioButton();
+            this.RdoBin = new System.Windows.Forms.RadioButton();
+            this.DlgSaveCjrFile = new System.Windows.Forms.SaveFileDialog();
+            this.DlgSaveBinFile = new System.Windows.Forms.SaveFileDialog();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnFill = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PicFontTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicEditFont)).BeginInit();
+            this.GrpRotation.SuspendLayout();
+            this.GrpFormat.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PicFontTable
+            // 
+            this.PicFontTable.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PicFontTable.Location = new System.Drawing.Point(13, 13);
+            this.PicFontTable.Name = "PicFontTable";
+            this.PicFontTable.Size = new System.Drawing.Size(514, 514);
+            this.PicFontTable.TabIndex = 0;
+            this.PicFontTable.TabStop = false;
+            this.PicFontTable.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.PicFontTable_MouseDoubleClick);
+            // 
+            // PicEditFont
+            // 
+            this.PicEditFont.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PicEditFont.Location = new System.Drawing.Point(549, 13);
+            this.PicEditFont.Name = "PicEditFont";
+            this.PicEditFont.Size = new System.Drawing.Size(194, 194);
+            this.PicEditFont.TabIndex = 1;
+            this.PicEditFont.TabStop = false;
+            this.PicEditFont.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PicEditFont_MouseClick);
+            // 
+            // GrpRotation
+            // 
+            this.GrpRotation.Controls.Add(this.RdoR270);
+            this.GrpRotation.Controls.Add(this.RdoR180);
+            this.GrpRotation.Controls.Add(this.RdoR90);
+            this.GrpRotation.Controls.Add(this.RdoR0);
+            this.GrpRotation.Location = new System.Drawing.Point(549, 387);
+            this.GrpRotation.Name = "GrpRotation";
+            this.GrpRotation.Size = new System.Drawing.Size(94, 128);
+            this.GrpRotation.TabIndex = 4;
+            this.GrpRotation.TabStop = false;
+            this.GrpRotation.Text = "回転";
+            // 
+            // RdoR270
+            // 
+            this.RdoR270.AutoSize = true;
+            this.RdoR270.Location = new System.Drawing.Point(7, 97);
+            this.RdoR270.Name = "RdoR270";
+            this.RdoR270.Size = new System.Drawing.Size(53, 16);
+            this.RdoR270.TabIndex = 3;
+            this.RdoR270.TabStop = true;
+            this.RdoR270.Text = "270度";
+            this.RdoR270.UseVisualStyleBackColor = true;
+            // 
+            // RdoR180
+            // 
+            this.RdoR180.AutoSize = true;
+            this.RdoR180.Location = new System.Drawing.Point(7, 74);
+            this.RdoR180.Name = "RdoR180";
+            this.RdoR180.Size = new System.Drawing.Size(53, 16);
+            this.RdoR180.TabIndex = 2;
+            this.RdoR180.TabStop = true;
+            this.RdoR180.Text = "180度";
+            this.RdoR180.UseVisualStyleBackColor = true;
+            // 
+            // RdoR90
+            // 
+            this.RdoR90.AutoSize = true;
+            this.RdoR90.Location = new System.Drawing.Point(7, 51);
+            this.RdoR90.Name = "RdoR90";
+            this.RdoR90.Size = new System.Drawing.Size(47, 16);
+            this.RdoR90.TabIndex = 1;
+            this.RdoR90.TabStop = true;
+            this.RdoR90.Text = "90度";
+            this.RdoR90.UseVisualStyleBackColor = true;
+            // 
+            // RdoR0
+            // 
+            this.RdoR0.AutoSize = true;
+            this.RdoR0.Checked = true;
+            this.RdoR0.Location = new System.Drawing.Point(7, 28);
+            this.RdoR0.Name = "RdoR0";
+            this.RdoR0.Size = new System.Drawing.Size(41, 16);
+            this.RdoR0.TabIndex = 0;
+            this.RdoR0.TabStop = true;
+            this.RdoR0.Text = "0度";
+            this.RdoR0.UseVisualStyleBackColor = true;
+            // 
+            // BtnWriteBack
+            // 
+            this.BtnWriteBack.Location = new System.Drawing.Point(549, 220);
+            this.BtnWriteBack.Name = "BtnWriteBack";
+            this.BtnWriteBack.Size = new System.Drawing.Size(75, 23);
+            this.BtnWriteBack.TabIndex = 0;
+            this.BtnWriteBack.Text = "←書戻し";
+            this.BtnWriteBack.UseVisualStyleBackColor = true;
+            this.BtnWriteBack.Click += new System.EventHandler(this.BtnWriteBack_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(666, 492);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(75, 23);
+            this.BtnSave.TabIndex = 5;
+            this.BtnSave.Text = "保存";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // GrpFormat
+            // 
+            this.GrpFormat.Controls.Add(this.RdoCjr);
+            this.GrpFormat.Controls.Add(this.RdoBin);
+            this.GrpFormat.Location = new System.Drawing.Point(556, 315);
+            this.GrpFormat.Name = "GrpFormat";
+            this.GrpFormat.Size = new System.Drawing.Size(185, 49);
+            this.GrpFormat.TabIndex = 3;
+            this.GrpFormat.TabStop = false;
+            this.GrpFormat.Text = "フォーマット";
+            // 
+            // RdoCjr
+            // 
+            this.RdoCjr.AutoSize = true;
+            this.RdoCjr.Location = new System.Drawing.Point(91, 19);
+            this.RdoCjr.Name = "RdoCjr";
+            this.RdoCjr.Size = new System.Drawing.Size(46, 16);
+            this.RdoCjr.TabIndex = 1;
+            this.RdoCjr.TabStop = true;
+            this.RdoCjr.Text = "CJR";
+            this.RdoCjr.UseVisualStyleBackColor = true;
+            // 
+            // RdoBin
+            // 
+            this.RdoBin.AutoSize = true;
+            this.RdoBin.Checked = true;
+            this.RdoBin.Location = new System.Drawing.Point(7, 19);
+            this.RdoBin.Name = "RdoBin";
+            this.RdoBin.Size = new System.Drawing.Size(42, 16);
+            this.RdoBin.TabIndex = 0;
+            this.RdoBin.TabStop = true;
+            this.RdoBin.Text = "BIN";
+            this.RdoBin.UseVisualStyleBackColor = true;
+            // 
+            // DlgSaveCjrFile
+            // 
+            this.DlgSaveCjrFile.Filter = "CJRファイル|*.cjr|すべてのファイル|*.*";
+            // 
+            // DlgSaveBinFile
+            // 
+            this.DlgSaveBinFile.Filter = "BINファイル|*.bin|すべてのファイル|*.*";
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(667, 220);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(75, 23);
+            this.BtnClear.TabIndex = 1;
+            this.BtnClear.Text = "全黒";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // BtnFill
+            // 
+            this.BtnFill.Location = new System.Drawing.Point(667, 259);
+            this.BtnFill.Name = "BtnFill";
+            this.BtnFill.Size = new System.Drawing.Size(75, 23);
+            this.BtnFill.TabIndex = 2;
+            this.BtnFill.Text = "全白";
+            this.BtnFill.UseVisualStyleBackColor = true;
+            this.BtnFill.Click += new System.EventHandler(this.BtnFill_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(693, 378);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ver 0.1.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(667, 403);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "(C)FIND 2024";
+            // 
+            // Main
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 536);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnFill);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.GrpFormat);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.BtnWriteBack);
+            this.Controls.Add(this.GrpRotation);
+            this.Controls.Add(this.PicEditFont);
+            this.Controls.Add(this.PicFontTable);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "フォントエディタ";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Main_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Main_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.PicFontTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicEditFont)).EndInit();
+            this.GrpRotation.ResumeLayout(false);
+            this.GrpRotation.PerformLayout();
+            this.GrpFormat.ResumeLayout(false);
+            this.GrpFormat.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PicFontTable;
+        private System.Windows.Forms.PictureBox PicEditFont;
+        private System.Windows.Forms.GroupBox GrpRotation;
+        private System.Windows.Forms.RadioButton RdoR270;
+        private System.Windows.Forms.RadioButton RdoR180;
+        private System.Windows.Forms.RadioButton RdoR90;
+        private System.Windows.Forms.RadioButton RdoR0;
+        private System.Windows.Forms.Button BtnWriteBack;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.GroupBox GrpFormat;
+        private System.Windows.Forms.RadioButton RdoCjr;
+        private System.Windows.Forms.RadioButton RdoBin;
+        private System.Windows.Forms.SaveFileDialog DlgSaveCjrFile;
+        private System.Windows.Forms.SaveFileDialog DlgSaveBinFile;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Button BtnFill;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ EditFont/Main.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EditFont
+{
+    public partial class Main : Form
+    {
+        const int ROM_SIZE = 2048;
+        const int CHARS = ROM_SIZE / 8;
+        const int TABLE_SCALE = 4;
+        const int EDIT_SCALE = 24;
+
+        private string[] fileName;
+        private byte[] fontRomData, editData;
+        private Bitmap bmpOrgTable, bmpViewTable, bmpEditFont;
+        private int fontRomData_head = -1;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            bmpEditFont = new Bitmap(8 * EDIT_SCALE, 8 * EDIT_SCALE);
+            bmpOrgTable = new Bitmap(8 * 16 * TABLE_SCALE, 8 * 16 * TABLE_SCALE);
+            editData = new byte[8];
+        }
+
+
+        private void Main_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Main_DragDrop(object sender, DragEventArgs e)
+        {
+            fileName = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            var info = new FileInfo(fileName[0]);
+            if (info.Length != ROM_SIZE || fileName.Length != 1)
+            {
+                System.Media.SystemSounds.Beep.Play();
+                return;
+            }
+
+            try
+            {
+                fontRomData = System.IO.File.ReadAllBytes(fileName[0]);
+            }
+            catch (Exception ex)
+            {
+                System.Media.SystemSounds.Beep.Play();
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            SetBmpOrgTable();
+            bmpViewTable = new Bitmap(bmpOrgTable);
+            this.PicFontTable.Image = bmpViewTable;
+
+            fontRomData_head = -1;
+            ClearEditData();
+        }
+
+
+        /// <summary>
+        /// フォント一覧を表示するためのBMP作成
+        /// </summary>
+        private void SetBmpOrgTable()
+        {
+            int index = 0;
+
+            for (int yy = 0; yy < (int)(CHARS / 16); ++yy)
+            {
+                for (int xx = 0; xx < 16; ++xx)
+                {
+                    for (int y = 0; y < 8; ++y)
+                    {
+                        for (int x = 0; x < 8; ++x)
+                        {
+                            var b = fontRomData[index];
+                            int j = b >> (7 - x);
+                            j &= 1;
+
+                            Color c = j == 1 ? Color.White : Color.Black;
+                            using (Graphics g = Graphics.FromImage(bmpOrgTable))
+                            {
+                                g.FillRectangle(
+                                    new SolidBrush(c), 
+                                    xx * 8 * TABLE_SCALE + x * TABLE_SCALE, 
+                                    yy * 8 * TABLE_SCALE + y * TABLE_SCALE,
+                                    TABLE_SCALE,
+                                    TABLE_SCALE);
+                            }
+
+                        }
+                        ++index;
+                    }
+                }
+            }
+            this.PicFontTable.Image = bmpOrgTable;
+        }
+
+
+
+
+
+        private void PicFontTable_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            { // 左クリック
+                int x = (int)(e.X / TABLE_SCALE);
+                int y = (int)(e.Y / TABLE_SCALE);
+
+                fontRomData_head = y / 8 * 8 * 16 + x / 8 * 8;
+
+                for (int i = 0; i < 8; ++i)
+                {
+                    editData[i] = fontRomData[fontRomData_head + i];
+                }
+
+                DrawFontData();
+
+                x /= 8;
+                y /= 8;
+
+                bmpViewTable = (Bitmap)bmpOrgTable.Clone();
+                using (Graphics g = Graphics.FromImage(bmpViewTable))
+                {
+                    g.DrawRectangle(
+                        new Pen(Color.Red), 
+                        x * 8 * TABLE_SCALE, 
+                        y * 8 * TABLE_SCALE, 
+                        8 * TABLE_SCALE, 
+                        8 * TABLE_SCALE);
+
+                }
+                this.PicFontTable.Image = bmpViewTable;
+                this.PicFontTable.Invalidate();
+            }
+
+        }
+
+        private void BtnWriteBack_Click(object sender, EventArgs e)
+        {
+            if (fontRomData_head == -1) return;
+
+            for (int i = 0; i < 8; ++i)
+            {
+                fontRomData[fontRomData_head + i] = editData[i];
+            }
+            fontRomData_head = -1;
+
+            SetBmpOrgTable();
+
+            this.PicFontTable.Image = bmpOrgTable;
+            this.PicFontTable.Invalidate();
+
+            ClearEditData();
+        }
+
+        private void PicEditFont_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (bmpEditFont == null || bmpOrgTable == null || fontRomData_head == -1)
+                return;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                int x = (int)(e.X / EDIT_SCALE);
+                int y = (int)(e.Y / EDIT_SCALE);
+
+                byte b = editData[y];
+                x = 7 - x;
+                int f = 1 << x;
+                var z = b ^ f;
+                editData[y] = (byte)z;
+
+                DrawFontData();
+            }
+        }
+
+
+        /// <summary>
+        /// 編集するフォントの表示
+        /// </summary>
+        private void DrawFontData()
+        {
+            int index = 0;
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    var b = editData[index];
+                    int j = b >> (7 - x);
+                    j &= 1;
+
+                    Color c = j == 1 ? Color.White : Color.Black;
+
+                    using (Graphics g = Graphics.FromImage(bmpEditFont))
+                    {
+                        g.FillRectangle(
+                            new SolidBrush(c), 
+                            x * EDIT_SCALE, 
+                            y * EDIT_SCALE,
+                            EDIT_SCALE,
+                            EDIT_SCALE);
+                    }
+                }
+                ++index;
+            }
+            DrawGrid();
+        }
+
+
+
+        /// <summary>
+        /// フォント編集用グリッド描画
+        /// </summary>
+        void DrawGrid()
+        {
+            using (Graphics g = Graphics.FromImage(bmpEditFont))
+            {
+                for (int y = 1; y < 8; ++y)
+                {
+                    g.DrawLine(
+                        new Pen(Color.White), 
+                        0, 
+                        y * EDIT_SCALE, 
+                        8 * EDIT_SCALE, 
+                        y * EDIT_SCALE);
+
+                }
+                for (int x = 1; x < 8; ++x)
+                {
+                    g.DrawLine(
+                        new Pen(Color.White), 
+                        x * EDIT_SCALE, 
+                        0, 
+                        x * EDIT_SCALE, 
+                        8 * EDIT_SCALE);
+                }
+                this.PicEditFont.Image = bmpEditFont;
+            }
+        }
+
+
+
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            if (fontRomData_head == -1) return;
+            
+            ClearEditData();
+            DrawGrid();
+        }
+
+        private void BtnFill_Click(object sender, EventArgs e)
+        {
+            if (fontRomData_head == -1) return;
+
+            for (int i = 0; i < 8; ++i)
+            {
+                editData[i] = 0xff;
+            }
+
+            using (Graphics g = Graphics.FromImage(bmpEditFont))
+            {
+                g.FillRectangle(
+                    new SolidBrush(Color.White), 
+                    0, 
+                    0, 
+                    8 * EDIT_SCALE, 
+                    8 * EDIT_SCALE);
+
+            }
+            this.PicEditFont.Image = bmpEditFont;
+            this.PicEditFont.Invalidate();
+
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            if (fontRomData == null) return;
+
+            if (this.RdoR0.Checked)
+            {
+                FileSave(fontRomData);
+            }
+            else if (this.RdoR90.Checked)
+            {
+                byte[] rotateData = new byte[ROM_SIZE];
+
+                for (int c = 0; c < CHARS; ++c)
+                {
+                    for (int x = 0; x < 8; ++x)
+                    {
+                        for (int t = 0; t < 8; ++t)
+                        {
+                            var b = fontRomData[c * 8 + t];
+                            int j = b >> (7 - x);
+                            j &= 1;
+
+                            rotateData[c * 8 + x] |= (byte)(j << t);
+                        }
+                    }
+                }
+                FileSave(rotateData);
+            }
+            else if (this.RdoR180.Checked)
+            {
+                byte[] rotateData = new byte[ROM_SIZE];
+
+                for (int c = 0; c < CHARS; ++c)
+                {
+                    for (int x = 0; x < 8; ++x)
+                    {
+                        var b = fontRomData[c * 8 + 7 - x];
+                        int d = 0;
+                        for (int i = 0; i < 8; i++)
+                        {
+                            int j = b >> i;
+                            j &= 1;
+                            d <<= 1;
+                            d += j;
+                        }
+                        rotateData[c * 8 + x] = (byte)d;
+                    }
+                }
+                FileSave(rotateData);
+            }
+            else if (this.RdoR270.Checked)
+            {
+                byte[] rotateData = new byte[ROM_SIZE];
+
+                for (int c = 0; c < CHARS; ++c)
+                {
+                    for (int x = 0; x < 8; ++x)
+                    {
+                        for (int t = 0; t < 8; ++t)
+                        {
+                            var b = fontRomData[c * 8 + t];
+                            int j = b >> x;
+                            j &= 1;
+
+                            rotateData[c * 8 + x] |= (byte)(j << (7 - t));
+                        }
+                    }
+                }
+                FileSave(rotateData);
+            }
+        }
+
+
+        /// <summary>
+        /// BINまたはCJRで保存
+        /// </summary>
+        /// <param name="data"></param>
+        void FileSave(byte[] data)
+        {
+            string fileName;
+
+            if (this.RdoBin.Checked)
+            {
+                // BINで保存
+                if (DlgSaveBinFile.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        System.IO.File.WriteAllBytes(DlgSaveBinFile.FileName, data);
+                    } catch (Exception ex)
+                    {
+                        System.Media.SystemSounds.Beep.Play();
+                        MessageBox.Show(ex.Message);
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                // CJRで保存
+                if (DlgSaveCjrFile.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = DlgSaveCjrFile.FileName;
+                    var cjr = new CjrFormat2();
+                    cjr.AddBinData(data, "FONTDATA", 0xd000, false);
+                    cjr.CloseCjrData();
+                    try
+                    {
+                        cjr.WriteCjrFile(fileName, CjrFormat2.WriteType.CJR);
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Media.SystemSounds.Beep.Play();
+                        MessageBox.Show(ex.Message);
+                        return;
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// エディット中のフォントデータをクリア
+        /// </summary>
+        void ClearEditData()
+        {
+            for (int i = 0; i < 8; ++i)
+            {
+                editData[i] = 0;
+            }
+
+            using (Graphics g = Graphics.FromImage(bmpEditFont))
+            {
+                g.FillRectangle(new SolidBrush(Color.Black), 0, 0, 8 * EDIT_SCALE, 8 * EDIT_SCALE);
+
+            }
+            this.PicEditFont.Image = bmpEditFont;
+            this.PicEditFont.Invalidate();
+        }
+
+    }
+}
